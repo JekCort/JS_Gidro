12042681c14d685495eb2805a34c74120a368e8e
Yevgen Kostenko <jekcort969@gmail.com>
2023-08-15 16:41:44 +0300

    Create search section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-$(function(){
-$('.banner-section__slider').slick({
-    dots:true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../images/arrow-left.svg" alt="Arrow left"></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../images/arrow-right.svg" alt="Arrow left"></button>'
-})
+$(function () {
+
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../images/arrow-left.svg" alt="Arrow left"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../images/arrow-right.svg" alt="Arrow left"></button>'
+    });
+
+    $('.search__tabs-item').on('click', function (e) {
+        e.preventDefault();
+
+        $('.search__tabs-item').removeClass('search__tabs-item-active');
+        $('.search__content-item').removeClass('search__content-item-active');
+
+        $(this).addClass('search__tabs-item-active');
+        $($(this).attr('href')).addClass('search__content-item-active');
+
+    })
+
 });
+
